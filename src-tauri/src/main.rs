acedd0c2ec169a8e8aeaeb572a7eb7d634921818
Fernando B <fernandobe+git@protonmail.com>
2022-10-09 20:13:05 -0500

    u

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,57 @@
   windows_subsystem = "windows"
 )]
 
+mod eip_plc;
+
+use eip_plc::{EipPlc, EipTagType};
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct MyState(EipPlc);
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+/*#[tauri::command]
+fn read_bool(tag: &str) ->  String {
+  let timeout = 100;//ms
+  let path="protocol=ab-eip&plc=controllogix&path=1,1&gateway=192.168.1.14&name=BaseBOOL&elem_count=1&elem_size=1";// YOUR TAG DEFINITION
+  let tag = RawTag::new(path, timeout).unwrap();
+  let status = tag.read(timeout);
+  assert!(status.is_ok());
+  let value: u8 = tag.get_value(0).unwrap();
+
+  return if value > 0 {
+    String::from("PLC BaseBOOL tag: true")
+  } else {
+    String::from("PLC BaseBOOL tag: false")
+  }
+}*/
+
+#[tauri::command]
+fn init_plc(plc: &mut tauri::State<MyState>) {
+  // let path="protocol=ab-eip&plc=controllogix&path=1,1&gateway=192.168.1.14&name=".to_string();
+  // let mut plc = &mut EipPlc::new(100, path);
+  // plc.add_tag("BaseBOOL".to_string(), EipTagType::Bool);
+
+  // plc.add_tag("BaseREAL".to_string(), EipTagType::Real);
+
+  plc.0.add_tag("BaseREAL".to_string(), EipTagType::Real);
+
+  //plc.add_tag("BaseREAL".to_string(), EipTagType::Real);
+}
+
 fn main() {
+
+  let path="protocol=ab-eip&plc=controllogix&path=1,1&gateway=192.168.1.14&name=".to_string();
+  let mut plc = EipPlc::new(100, path);
+  plc.add_tag("BaseBOOL".to_string(), EipTagType::Bool);
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .manage(MyState(plc))
+      .invoke_handler(tauri::generate_handler![greet, init_plc])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { Chart, ChartDataset, ChartOptions } from 'chart.js';
+import 'chartjs-adapter-luxon';
+import StreamingPlugin from 'chartjs-plugin-streaming';
+import { invoke } from "@tauri-apps/api/tauri";
+
+Chart.register(StreamingPlugin);
+
+interface Point {
+  x: number,
+  y: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'plot-test';
+
+  greetingMessage = "";
+  booleanResult = "";
+  generatedData: {[key: string]: Point[]} = {
+    "Dataset 1": [],
+    "Dataset 2": []
+  };
+  currentIndex = 0;
+
+
+  constructor() {
+
+    invoke("init_plc").then(r => console.log("plc initialized"));
+
+    setInterval(() => {
+      this.generateData()
+    }, 1000)
+  }
+
+  public datasets: ChartDataset[] = [{
+    label: 'Dataset 1',
+    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    borderColor: 'rgb(255, 99, 132)',
+    borderDash: [8, 4],
+    fill: false,
+    data: []
+  }, {
+    label: 'Dataset 2',
+    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+    borderColor: 'rgb(54, 162, 235)',
+    cubicInterpolationMode: 'monotone',
+    fill: false,
+    data: []
+  }];
+
+  public options: ChartOptions = {
+    scales: {
+      x: {
+        type: 'realtime',
+        realtime: {
+          delay: 2000,
+          onRefresh: (chart: Chart) => {
+            chart.data.datasets.forEach((dataset: ChartDataset) => {
+
+              // grab correct data, if it exists
+              if (this.generatedData[dataset.label!].length == 0) return;
+
+              let currData = this.generatedData[dataset.label!]
+              console.log(`Current index ${this.currentIndex}`)
+              console.log(`Current label ${dataset.label}`)
+              console.log(currData)
+              console.log(currData[0])
+              // console.log(`One item ${Object.values(currData)[this.currentIndex]}`)
+
+              dataset.data.push({
+                x: currData[this.currentIndex].x,
+                y: currData[this.currentIndex].y
+              });
+
+
+
+              // dataset.data.push({
+              //   x: Date.now(),
+              //   y: Math.random()
+              // });
+              //
+              // console.log(dataset.label);
+            });
+
+            this.currentIndex++;
+          }
+        }
+      }
+    }
+  };
+
+
+  generateData(): void {
+    // let tag: string = "yo";
+    // invoke<string>("read_bool", { tag }).then((text:string) => {
+    //   this.booleanResult = text;
+    // });
+
+    this.generatedData['Dataset 1'].push({"x": Date.now(), "y": Math.random() * 5});
+    this.generatedData['Dataset 2'].push({"x": Date.now(), "y": (Math.random() * 10) + 5});
+
+    //console.log(this.generatedData)
+  }
+
+
+  greet(name: string): void {
+    invoke<string>("greet", { name }).then((text:string) => {
+      this.greetingMessage = text;
+    });
+  }
 }
